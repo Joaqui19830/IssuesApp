b4b44c3f858ceb6d9b9256854141fa4284680229
Joaquin <joaqui2608.jc12@gmail.com>
2024-02-06 19:21:52 -0300

    Final de app de github

--- src/api/githubApi.ts
+++ src/api/githubApi.ts
@@ -3,7 +3,6 @@ import axios from "axios";
 export const githubApi = axios.create({
   baseURL: "https://api.github.com/repos/facebook/react",
   headers: {
-    Authorization:
-      "Barear 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",
+    // Authorization:
   },
 });
